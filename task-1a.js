01045c84a21b9ac40658e355a0a25ac68d8fe6b6
Vlad <ramusvlad@ya.ru>
2020-04-20 18:34:35 +0300

    Added the function generating several tasks

--- task-1a.js
+++ task-1a.js
@@ -1,3 +1,7 @@
+function randomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
 function getDigitSum(number) {
     let s = 0;
     while (number > 0) {
@@ -15,48 +19,109 @@ function generateResult(number) {
 }
 
 let form = document.querySelector('form');
+let submitButton = document.querySelector('.submit-button');
+submitButton.disabled = true;
 let key;
-// Math.seed = key.value;
+let varCount;
 
-Math.seededRandom = function(max, min) {
-    max = max || 1;
-    min = min || 0;
- 
-    Math.seed = (Math.seed * 9301 + 49297) % 233280;
-    var rnd = Math.seed / 233280;
- 
-    return Math.floor(min + rnd * (max - min));
-}
-
-function randomInteger(min, max) {
-    return Math.seededRandom(min, max);
-}
 
 let taskID = document.querySelector('.task-id-text');
 let supField = document.querySelector('.supremum');
 
+// let keyField = document.querySelector('.key-field');
+// let countField = document.querySelector('.key-field');
+
+// keyField.oninput = function () {
+//   if (keyField.value.length == 5) {
+//     submitButton.disabled = false;
+//   }
+// };
+
+// countField.oninput = function () {
+//   if (countField.value.length < 0 || countField.value.length > 30) {
+//     submitButton.disabled = false;
+//   }
+// };
+
 form.onclick = function() {
+    submitButton.disabled = false;
     key = document.querySelector('.key-field');
-    taskID.textContent = '';
-    //.textContent = '';
-    Math.seed = key.value;
+    varCount = document.querySelector('.count-field');
+    Math.seedrandom(key.value);
+}
+
+TEXT = "На вход алгоритма подаётся натуральное число N. Алгоритм строит по нему новое число R следующим образом.\n" +
+    "  1) Строится двоичная запись числа N.\n" +
+    "  2) К этой записи дописываются справа ещё два разряда по следующему правилу:\n" +
+    "    а) складываются все цифры двоичной записи, и остаток от деления суммы на 2 " + 
+    "дописывается в конец числа (справа). Например, запись 11100 преобразуется в запись 111001;\n" +
+    "    б) над этой записью производятся те же действия — справа дописывается остаток от деления суммы цифр на 2.\n" +
+    "Полученная таким образом запись (в ней на два разряда больше, чем в записи исходного числа N) является двоичной записью искомого " +
+    "числа R. Укажите минимальное число R, которое превышает 100 и может являться результатом работы алгоритма. " + 
+    "В ответе это число запишите в десятичной системе.";
+
+function formatTaskText (supremum) {
+    return TEXT.replace('превышает 100', 'превышает ' + supremum.toString());;
+}
+
+function addTask (text, varVal, ans) {
+    let content = document.querySelector('#content');
+    
+    let taskContent = document.createElement('div');
+    taskContent.setAttribute('class', 'task-content');
+    
+    let pVar = document.createElement('p');
+    let emVar = document.createElement('em');
+    emVar.textContent = 'Вариант ' + ' ' + varVal.toString(); //
+    pVar.appendChild(emVar);
+
+    let pTask = document.createElement('p');
+    pTask.setAttribute('class', 'task-text');
+    pTask.textContent = text;
+
+    let spoiler = document.createElement('details');
+    spoiler.setAttribute('class', 'answer-spoiler');
+
+    let summary = document.createElement('summary');
+    summary.setAttribute('class', 'answer');
+    summary.textContent = 'Ответ';
+
+    let answer = document.createElement('p');
+    answer.textContent = ans;
+
+    spoiler.appendChild(summary);
+    spoiler.appendChild(answer);
+
+    taskContent.appendChild(document.createElement('hr'));
+    taskContent.appendChild(pVar);
+    taskContent.appendChild(pTask);
+    taskContent.appendChild(spoiler);
+
+    content.appendChild(taskContent);
+
 }
 
 form.onsubmit = function(evt) {
     evt.preventDefault();
-    // console.log('Generate!');
-    let number = randomInteger(20, 30);
-    // console.log("number:", number);
-    let answer = parseInt(generateResult(number), 2);
-    // console.log("answer:", answer);
-    let previous = parseInt(generateResult(number - 1), 2);
-    // console.log("previous: ", previous);
-    let sup = randomInteger(previous, answer - 1);
-    // console.log("sup:", sup);
-    let ansField = document.querySelector('.answer');
-    ansField.textContent = answer.toString();
-    supField.textContent = sup.toString();
-    taskID.textContent = '';
-    taskID.textContent = 'ID: #' + key.value.toString();
-    Math.seed = key.value;
+
+    let flag = document.getElementsByClassName('task-content').length;
+
+    while (flag > 0) {
+        let div = document.querySelector('.task-content');
+        div.remove();
+        flag = document.getElementsByClassName('task-content').length;
+    }
+
+    for (let i = 0; i < parseInt(varCount.value); ++i) {
+        let number = randomInteger(20, 30);
+        let answer = parseInt(generateResult(number), 2);
+        let previous = parseInt(generateResult(number - 1), 2);
+        let sup = randomInteger(previous, answer - 1);
+        let text = formatTaskText(sup);
+        console.log(text);
+        addTask(text, i + 1, answer);
+    }
+    submitButton.disabled = true;
+
+    Math.seedrandom(key.value);
 }
